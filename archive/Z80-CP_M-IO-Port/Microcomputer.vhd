9ce85f5905acb7d2e0b69b9cd3c57150f1363e47
Doug Gilliland <doug@douglasgilliland.com>
2017-01-25 12:01:16 -0500

    stuff

--- archive/Z80-CP_M-IO-Port/Microcomputer.vhd
+++ archive/Z80-CP_M-IO-Port/Microcomputer.vhd
@@ -1,17 +1,9 @@
 -- This file is copyright by Grant Searle 2014
--- You are free to use this file in your own projects but must never charge for it nor use it without
--- acknowledgement.
--- Please ask permission from Grant Searle before republishing elsewhere.
--- If you use this file or any part of it, please add an acknowledgement to myself and
--- a link back to my main web site http://searle.hostei.com/grant/    
--- and to the "multicomp" page at http://searle.hostei.com/grant/Multicomp/index.html
+-- Grant Searle's web site http://searle.hostei.com/grant/    
+-- Grant Searle's "multicomp" page at http://searle.hostei.com/grant/Multicomp/index.html
 --
--- Please check on the above web pages to see if there are any updates before using this file.
--- If for some reason the page is no longer available, please search for "Grant Searle"
--- on the internet to see if I have moved to another web hosting service.
+-- Changes to this code by Doug Gilliland 2017
 --
--- Grant Searle
--- eMail address available on my main web page link above.
 
 library ieee;
 use ieee.std_logic_1164.all;
