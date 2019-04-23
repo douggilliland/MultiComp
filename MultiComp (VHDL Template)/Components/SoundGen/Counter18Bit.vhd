aed7b3fc2bcc1765430b80c1371d8eab2b39ac1f
Doug Gilliland <doug@douglasgilliland.com>
2019-04-22 23:15:11 -0400

    Sound cut 2 done

--- MultiComp (VHDL Template)/Components/SoundGen/Counter18Bit.vhd
+++ MultiComp (VHDL Template)/Components/SoundGen/Counter18Bit.vhd
@@ -9,24 +9,25 @@ use ieee.std_logic_unsigned.all;
 
 ----------------------------------------------------
 
-entity Counter16Bit is
+entity Counter18Bit is
 
 port(	
-	clock:	in std_logic;	-- 50 MHz clock
-	Q:	out std_logic			-- Most Signif Bit of counter
+	clock:		in std_logic;	-- 50 MHz clock
+	selectTap:	in std_logic_vector(2 downto 0);
+	Q:				out std_logic			-- Most Signif Bit of counter
 );
-end Counter16Bit;
+end Counter18Bit;
 
 ----------------------------------------------------
 
-architecture behv of Counter16Bit is		 	  
+architecture behv of Counter18Bit is		 	  
 	
-	signal Pre_Q: std_logic_vector(15 downto 0);
+	signal Pre_Q: std_logic_vector(17 downto 0);
 	
 
 begin
 
-    -- behavior describes the Counter16Bit
+    -- behavior describes the Counter18Bit
 
     process(clock, Pre_Q)
     begin
@@ -36,6 +37,6 @@ begin
     end process;	
 	
     -- concurrent assignment statement
-    Q <= Pre_Q(15);
+    Q <= ((Pre_Q(15) and selectTap(2)) or (Pre_Q(16) and selectTap(1)) or (Pre_Q(17) and selectTap(0)));
 
 end behv;
