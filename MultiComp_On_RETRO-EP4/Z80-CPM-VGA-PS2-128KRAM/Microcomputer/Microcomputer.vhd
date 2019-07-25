f61ab2f6a81cfd2b0bc71fd9bea7f39782b85ee9
Doug Gilliland <doug@douglasgilliland.com>
2019-07-25 09:52:41 -0400

    Got CP/M working on the RETRO-EP4 card

--- MultiComp_On_RETRO-EP4/Z80-CPM-VGA-PS2-128KRAM/Microcomputer/Microcomputer.vhd
+++ MultiComp_On_RETRO-EP4/Z80-CPM-VGA-PS2-128KRAM/Microcomputer/Microcomputer.vhd
@@ -2,7 +2,7 @@
 -- You are free to use this file in your own projects but must never charge for it nor use it without
 -- acknowledgement.
 --
--- Changes by Doug Gilliland 2017
+-- Changes by Doug Gilliland 2017-2019
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -11,53 +11,47 @@ use  IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity Microcomputer is
 	port(
-		n_reset		: in std_logic;
+		n_reset		: in std_logic :='1';
 		clk			: in std_logic;
-
+		-- External SRAM
 		sramData		: inout std_logic_vector(7 downto 0);
-		sramAddress	: out std_logic_vector(16 downto 0);
-		n_sRamWE		: out std_logic;
-		n_sRamCS		: out std_logic;
-		n_sRamOE		: out std_logic;
-		
+		sramAddress	: out std_logic_vector(18 downto 0);
+		n_sRamWE		: out std_logic :='1';
+		n_sRamCS		: out std_logic :='1';
+		n_sRamOE		: out std_logic :='1';
+		-- Serial Port
 		rxd1			: in std_logic;
-		txd1			: out std_logic;
+		txd1			: out std_logic :='1';
 		rts1			: out std_logic;
-
-		-- rxd2			: in std_logic;
-		-- txd2			: out std_logic;
-		-- rts2			: out std_logic;
-		
-		videoSync	: out std_logic;
-		video			: out std_logic;
-
-		videoR0		: out std_logic;
-		videoG0		: out std_logic;
-		videoB0		: out std_logic;
-		videoR1		: out std_logic;
-		videoG1		: out std_logic;
-		videoB1		: out std_logic;
-		hSync			: out std_logic;
-		vSync			: out std_logic;
-
+		-- Video RGB
+		videoR0		: out std_logic :='0';
+		videoG0		: out std_logic :='0';
+		videoB0		: out std_logic :='0';
+		videoR1		: out std_logic :='0';
+		videoG1		: out std_logic :='0';
+		videoB1		: out std_logic :='0';
+		hSync			: out std_logic :='1';
+		vSync			: out std_logic :='1';
+		-- PS/2 Keyboard
 		ps2Clk		: inout std_logic;
 		ps2Data		: inout std_logic;
-
-		sdCS			: out std_logic;
-		sdMOSI		: out std_logic;
+		-- SD Card
+		sdCS			: out std_logic :='1';
+		sdMOSI		: out std_logic :='1';
 		sdMISO		: in std_logic;
-		sdSCLK		: out std_logic;
+		sdSCLK		: out std_logic :='1';
+		
 		driveLED		: out std_logic :='1';
-		ledOut8		: out std_logic_vector(7 downto 0);
-		J6IO8			: out std_logic_vector(7 downto 0);
-		J8IO8			: out std_logic_vector(7 downto 0)
+		ledOut8		: out std_logic_vector(7 downto 0)
+		--J6IO8			: out std_logic_vector(7 downto 0);
+		--J8IO8			: out std_logic_vector(7 downto 0)
 	);
 end Microcomputer;
 
 architecture struct of Microcomputer is
 
-	signal n_WR							: std_logic;
-	signal n_RD							: std_logic;
+	signal n_WR							: std_logic :='1';
+	signal n_RD							: std_logic :='1';
 	signal cpuAddress					: std_logic_vector(15 downto 0);
 	signal cpuDataOut					: std_logic_vector(7 downto 0);
 	signal cpuDataIn					: std_logic_vector(7 downto 0);
@@ -82,8 +76,6 @@ architecture struct of Microcomputer is
 	signal n_int2						: std_logic :='1';	
 	
 	signal n_externalRamCS			: std_logic :='1';
-	signal n_internalRam1CS			: std_logic :='1';
-	signal n_internalRam2CS			: std_logic :='1';
 	signal n_basRomCS					: std_logic :='1';
 	signal n_interface1CS			: std_logic :='1';
 	signal n_interface2CS			: std_logic :='1';
@@ -98,25 +90,26 @@ architecture struct of Microcomputer is
 	signal cpuClock					: std_logic;
 	signal serialClock				: std_logic;
 	signal sdClock						: std_logic;	
---CPM
-	signal n_RomActive : std_logic := '0';
+--CP/M
+	signal n_RomActive 				: std_logic := '0';
 
 begin
 
---CPM
+--CP/M
 -- Disable ROM if out 38. Re-enable when (asynchronous) reset pressed
-process (n_ioWR, n_reset) begin
-if (n_reset = '0') then
-n_RomActive <= '0';
-elsif (rising_edge(n_ioWR)) then
-if cpuAddress(7 downto 0) = "00111000" then -- $38
-n_RomActive <= '1';
-end if;
-end if;
+process (n_ioWR, n_reset) 
+	begin
+		if (n_reset = '0') then
+			n_RomActive <= '0';
+		elsif (rising_edge(n_ioWR)) then
+		if cpuAddress(7 downto 0) = "00111000" then -- $38
+			n_RomActive <= '1';
+		end if;
+	end if;
 end process;
 
 -- ____________________________________________________________________________________
--- CPU CHOICE GOES HERE
+-- Z80 CPU
 cpu1 : entity work.t80s
 generic map(mode => 1, t2write => 1, iowait => 0)
 port map(
@@ -136,7 +129,7 @@ port map(
 );
 
 -- ____________________________________________________________________________________
--- ROM GOES HERE	
+-- BASIC ROM
 
 rom1 : entity work.Z80_CPM_BASIC_ROM -- 8KB BASIC and CP/M boot
 port map(
@@ -146,17 +139,16 @@ port map(
 );
 
 -- ____________________________________________________________________________________
--- External RAM GOES HERE
-
+-- External RAM
+sramAddress(18 downto 16) <= "000";
 sramAddress(15 downto 0) <= cpuAddress(15 downto 0);
-sramAddress(16) <= '1';
 sramData <= cpuDataOut when n_memWR='0' else (others => 'Z');
 n_sRamWE <= n_memWR or n_externalRamCS;
 n_sRamOE <= n_memRD or n_externalRamCS;
 n_sRamCS <= n_externalRamCS;
 
 -- ____________________________________________________________________________________
--- INPUT/OUTPUT DEVICES GO HERE	
+-- INPUT/OUTPUT DEVICES
 
 io1 : entity work.bufferedUART	-- First Serial port
 port map(
@@ -176,7 +168,7 @@ port map(
 	n_rts => rts1
 );
 
-io2 : entity work.SBCTextDisplayRGB	-- VGA/Composite output
+io2 : entity work.SBCTextDisplayRGB	-- VGA output
 
 port map (
 	n_reset => n_reset,
@@ -192,10 +184,6 @@ port map (
 	videoB0 => videoB0,
 	videoB1 => videoB1,
 	
-	-- Monochrome video signals (when using TV timings only)
-	sync => videoSync,
-	video => video,
-	
 	n_wr => n_interface2CS or n_ioWR,
 	n_rd => n_interface2CS or n_ioRD,
 	n_int => n_int2,
@@ -206,30 +194,30 @@ port map (
 	ps2Data => ps2Data
 );
 
-latchIO0 : entity work.OUT_LATCH	--Output LatchIO
-port map(
-	clear => n_reset,
-	clock => clk,
-	load => n_J6IOCS,
-	dataIn8 => cpuDataOut,
-	latchOut => J6IO8
-);
-
-latchIO1 : entity work.OUT_LATCH	--Output LatchIO
-port map(
-	clear => n_reset,
-	clock => clk,
-	load => n_J8IOCS,
-	dataIn8 => cpuDataOut,
-	latchOut => J8IO8
-);
+--latchIO0 : entity work.OUT_LATCH	--Output LatchIO
+--port map(
+--	clear => n_reset,
+--	clock => clk,
+--	load => n_J6IOCS,
+--	dataIn8 => cpuDataOut,
+--	latchOut => J6IO8
+--);
+--
+--latchIO1 : entity work.OUT_LATCH	--Output LatchIO
+--port map(
+--	clear => n_reset,
+--	clock => clk,
+--	load => n_J8IOCS,
+--	dataIn8 => cpuDataOut,
+--	latchOut => J8IO8
+--);
 
 latchLED : entity work.OUT_LATCH	--Output LatchIO
 port map(
 	clear => n_reset,
 	clock => clk,
 	load => n_LEDCS,
-	dataIn8 => cpuDataOut,
+	dataIn8 => not cpuDataOut,
 	latchOut => ledOut8
 );
 
@@ -250,14 +238,14 @@ port map(
 );
 
 -- ____________________________________________________________________________________
--- MEMORY READ/WRITE LOGIC GOES HERE
+-- MEMORY READ/WRITE LOGIC
 n_ioWR <= n_WR or n_IORQ;
 n_memWR <= n_WR or n_MREQ;
 n_ioRD <= n_RD or n_IORQ;
 n_memRD <= n_RD or n_MREQ;
 
 -- ____________________________________________________________________________________
--- CHIP SELECTS GO HERE
+-- CHIP SELECTS
 n_basRomCS <= '0' when cpuAddress(15 downto 13)   = "000" and n_RomActive = '0' else '1'; --8K at bottom of memory
 n_interface1CS <= '0' when cpuAddress(7 downto 1) = "1000000" and (n_ioWR='0' or n_ioRD = '0') else '1'; -- 2 Bytes $80-$81
 n_interface2CS <= '0' when cpuAddress(7 downto 1) = "1000001" and (n_ioWR='0' or n_ioRD = '0') else '1'; -- 2 Bytes $82-$83
@@ -268,20 +256,17 @@ n_J8IOCS <= '0' when cpuAddress(7 downto 1)       = "1000100" and (n_ioWR='0' or
 n_externalRamCS<= not n_basRomCS;
 
 -- ____________________________________________________________________________________
--- BUS ISOLATION GOES HERE
+-- BUS ISOLATION
 cpuDataIn <=
 interface1DataOut when n_interface1CS = '0' else	-- UART 1
-interface2DataOut when n_interface2CS = '0' else	-- UART 2
+interface2DataOut when n_interface2CS = '0' else	-- UART 2 (VGA Display)
 sdCardDataOut when n_sdCardCS = '0' else				-- SD Card
 basRomData when n_basRomCS = '0' else
-internalRam1DataOut when n_internalRam1CS= '0' else
 sramData when n_externalRamCS= '0' else
 x"FF";
 
--- n_ChipSS <= n_PIOCS;
-
 -- ____________________________________________________________________________________
--- SYSTEM CLOCKS GO HERE
+-- SYSTEM CLOCKS
 -- SUB-CIRCUIT CLOCK SIGNALS
 serialClock <= serialClkCount(15);
 process (clk)
@@ -319,7 +304,7 @@ end if;
 -- 9600 201
 -- 4800 101
 -- 2400 50
-serialClkCount <= serialClkCount + 2416;
+serialClkCount <= serialClkCount + 2416;		-- 115200 baud serial port
 end if;
 end process;
 end;
--- MultiComp_On_RETRO-EP4/Z80-CPM-VGA-PS2-128KRAM/Microcomputer/output_files/Microcomputer.cdf
+++ MultiComp_On_RETRO-EP4/Z80-CPM-VGA-PS2-128KRAM/Microcomputer/output_files/Microcomputer.cdf
@@ -1,13 +1,13 @@
-/* Quartus II 64-Bit Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition */
+/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition */
 JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(EPCS4) Path("C:/Users/DGilliland/Documents/GitHub/MultiComp/Z80-CPM-VGA-PS2-128KRAM/Microcomputer/output_files/") File("Microcomputer.pof") MfrSpec(OpMask(1) Child_OpMask(1 1));
+		Device PartName(EP4CE6E22) Path("C:/Users/dgilliland/Documents/GitHub/MultiComp/MultiComp_On_RETRO-EP4/Z80-CPM-VGA-PS2-128KRAM/Microcomputer/output_files/") File("Microcomputer.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
 AlteraBegin;
-	ChainType(asc);
+	ChainType(JTAG);
 AlteraEnd;
