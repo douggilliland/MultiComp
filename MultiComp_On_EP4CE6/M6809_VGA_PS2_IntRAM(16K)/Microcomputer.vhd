626f441e365e5ed2420d80773ca3a21ed19c94ab
Doug Gilliland <doug@douglasgilliland.com>
2019-09-03 15:39:16 -0400

    Built EP4CE6 version of M6809 CPU

--- MultiComp_On_EP4CE6/M6809_VGA_PS2_IntRAM(16K)/Microcomputer.vhd
+++ MultiComp_On_EP4CE6/M6809_VGA_PS2_IntRAM(16K)/Microcomputer.vhd
@@ -12,75 +12,92 @@ use  IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity Microcomputer is
 	port(
-		n_reset		: in std_logic;
-		clk			: in std_logic;
+		i_n_reset		: in std_logic;
+		i_clk_50			: in std_logic;
 
-		o_Video_Red	: out std_logic;
-		o_Video_Grn	: out std_logic;
-		o_Video_Blu	: out std_logic;
-		hSync			: out std_logic;
-		vSync			: out std_logic;
+		o_vid_red	: out std_logic;
+		o_vid_grn	: out std_logic;
+		o_vid_blu	: out std_logic;
+		o_vid_hSync	: out std_logic;
+		o_vid_vSync	: out std_logic;
 
-		ps2Clk		: inout std_logic;
-		ps2Data		: inout std_logic
+		i_ps2Clk		: inout std_logic;
+		i_ps2Data		: inout std_logic
 	);
 end Microcomputer;
 
 architecture struct of Microcomputer is
 
-	signal n_WR							: std_logic;
-	signal cpuAddress					: std_logic_vector(15 downto 0);
-	signal cpuDataOut					: std_logic_vector(7 downto 0);
-	signal cpuDataIn					: std_logic_vector(7 downto 0);
 
-	signal basRomData					: std_logic_vector(7 downto 0);
-	signal interface1DataOut		: std_logic_vector(7 downto 0);
-	signal internalRam1DataOut		: std_logic_vector(7 downto 0);
+	signal w_n_WR							: std_logic;
+	signal w_cpuAddress					: std_logic_vector(15 downto 0);
+	signal w_cpuDataOut					: std_logic_vector(7 downto 0);
+	signal w_cpuDataIn					: std_logic_vector(7 downto 0);
 
-	signal n_memWR						: std_logic :='1';
-	signal n_basRomCS					: std_logic :='1';
-	signal n_videoInterfaceCS		: std_logic :='1';
-	signal n_internalRamCS			: std_logic :='1';
+	signal w_basRomData					: std_logic_vector(7 downto 0);
+	signal w_interface1DataOut		: std_logic_vector(7 downto 0);
+	signal w_internalRam1DataOut		: std_logic_vector(7 downto 0);
 
-	signal cpuClkCount				: std_logic_vector(5 downto 0); 
-	signal cpuClock					: std_logic;
-	signal videoR1						: std_logic;
-	signal videoR0						: std_logic;
-	signal videoG1						: std_logic;
-	signal videoG0						: std_logic;
-	signal videoB1						: std_logic;
-	signal videoB0						: std_logic;
+	signal w_n_memWR						: std_logic :='1';
+	signal w_n_basRomCS					: std_logic :='1';
+	signal w_n_videoInterfaceCS		: std_logic :='1';
+	signal w_n_internalRamCS			: std_logic :='1';
+
+	signal w_cpuClkCount				: std_logic_vector(5 downto 0); 
+	signal w_cpuClock					: std_logic;
+	
+	signal w_videoR0					: std_logic := '0';
+	signal w_videoR1					: std_logic := '0';
+	signal w_videoG0					: std_logic := '0';
+	signal w_videoG1					: std_logic := '0';
+	signal w_videoB0					: std_logic := '0';
+	signal w_videoB1					: std_logic := '0';
 	
 begin
 
-	o_Video_Red <= videoR1 or videoR0;
-	o_Video_Grn <= videoG1 or videoG0;
-	o_Video_Blu <= videoB1 or videoB0;
+	o_vid_red <= w_videoR1 or w_videoR0;
+	o_vid_grn <= w_videoG1 or w_videoG0;
+	o_vid_blu <= w_videoB1 or w_videoB0;
+	
+	-- ____________________________________________________________________________________
+	-- CHIP SELECTS
+	w_n_basRomCS <= '0'				when w_cpuAddress(15 downto 13) 	= "111" 					else '1'; 	-- 8K at top of memory
+	w_n_videoInterfaceCS <= '0'	when w_cpuAddress(15 downto 1) 	= "111111111101000" 	else '1'; 	-- 2 bytes FFD0-FFD1
+	w_n_internalRamCS <= '0'		when w_cpuAddress(15 downto 14) 	= "00" 					else '1';	-- 16K at bottom of memory
+	
+	-- ____________________________________________________________________________________
+	-- BUS ISOLATION
+	-- Order matters since BASIC overlaps I/O chip selects
+	w_cpuDataIn <=
+		w_interface1DataOut when w_n_videoInterfaceCS = '0' else
+		w_basRomData when w_n_basRomCS = '0' else
+		w_internalRam1DataOut when w_n_internalRamCS = '0' else
+		x"FF";
 	
 	-- ____________________________________________________________________________________
 	-- CPU CHOICE GOES HERE
 	cpu1 : entity work.cpu09
 		port map(
-			clk => not(cpuClock),
-			rst => not n_reset,
-			rw => n_WR,
-			addr => cpuAddress,
-			data_in => cpuDataIn,
-			data_out => cpuDataOut,
-			halt => '0',
-			hold => '0',
-			irq => '0',
-			firq => '0',
-			nmi => '0'
+			clk 		=> not(w_cpuClock),
+			rst 		=> not i_n_reset,
+			rw 		=> w_n_WR,
+			addr 		=> w_cpuAddress,
+			data_in 	=> w_cpuDataIn,
+			data_out => w_cpuDataOut,
+			halt 		=> '0',
+			hold 		=> '0',
+			irq 		=> '0',
+			firq 		=> '0',
+			nmi 		=> '0'
 		); 
 	
 	-- ____________________________________________________________________________________
 	-- ROM GOES HERE	
 	rom1 : entity work.M6809_EXT_BASIC_ROM -- 8KB BASIC
 		port map(
-			address => cpuAddress(12 downto 0),
-			clock => clk,
-			q => basRomData
+			address 	=> w_cpuAddress(12 downto 0),
+			clock 	=> i_clk_50,
+			q 			=> w_basRomData
 		);
 	
 	-- ____________________________________________________________________________________
@@ -89,11 +106,11 @@ begin
  	ram1: entity work.InternalRam16K
 		port map
 		(
-			address => cpuAddress(13 downto 0),
-			clock => clk,
-			data => cpuDataOut,
-			wren => not(n_memWR or n_internalRamCS),
-			q => internalRam1DataOut
+			address 	=> w_cpuAddress(13 downto 0),
+			clock 	=> i_clk_50,
+			data 		=> w_cpuDataOut,
+			wren 		=> not(w_n_memWR or w_n_internalRamCS),
+			q 			=> w_internalRam1DataOut
 		);
 	
 	-- ____________________________________________________________________________________
@@ -101,65 +118,47 @@ begin
 
 	io1 : entity work.SBCTextDisplayRGB
 		port map (
-			n_reset => n_reset,
-			clk => clk,
-			
-			-- RGB CompVideo signals
-			hSync => hSync,
-			vSync => vSync,
-			videoR0 => videoR0,
-			videoR1 => videoR1,
-			videoG0 => videoG0,
-			videoG1 => videoG1,
-			videoB0 => videoB0,
-			videoB1 => videoB1,
-			
-			n_wr => n_videoInterfaceCS or cpuClock or n_WR,
-			n_rd => n_videoInterfaceCS or cpuClock or (not n_WR),
-			regSel => cpuAddress(0),
-			dataIn => cpuDataOut,
-			dataOut => interface1DataOut,
-			ps2Clk => ps2Clk,
-			ps2Data => ps2Data
+			n_reset 	=> i_n_reset,
+			clk 		=> i_clk_50,
+			n_wr 		=> w_n_videoInterfaceCS or w_cpuClock or w_n_WR,
+			n_rd 		=> w_n_videoInterfaceCS or w_cpuClock or (not w_n_WR),
+			regSel 	=> w_cpuAddress(0),
+			dataIn 	=> w_cpuDataOut,
+			dataOut 	=> w_interface1DataOut,
+			-- VGA Video signals
+			hSync 	=> o_vid_hSync,
+			vSync 	=> o_vid_vSync,
+			videoR0 	=> w_videoR0,
+			videoR1 	=> w_videoR1,
+			videoG0 	=> w_videoG0,
+			videoG1 	=> w_videoG1,
+			videoB0 	=> w_videoB0,
+			videoB1 	=> w_videoB1,
+			-- PS/2 Keyboard
+			ps2Clk 	=> i_ps2Clk,
+			ps2Data 	=> i_ps2Data
 		);
 	
 	-- ____________________________________________________________________________________
 	-- MEMORY READ/WRITE LOGIC GOES HERE
-	n_memWR <= not(cpuClock) nand (not n_WR);
-	
-	-- ____________________________________________________________________________________
-	-- CHIP SELECTS GO HERE
-	n_basRomCS <= '0' when cpuAddress(15 downto 13) = "111" else '1'; --8K at top of memory
-	n_videoInterfaceCS <= '0' when cpuAddress(15 downto 1) = "111111111101000" else '1'; -- 2 bytes FFD0-FFD1
-	n_internalRamCS <= '0' when cpuAddress(15 downto 14) = "00" else '1';
-	
-	-- ____________________________________________________________________________________
-	-- BUS ISOLATION GOES HERE
-	-- Order matters since SRAM overlaps I/O chip selects
-	cpuDataIn <=
-	interface1DataOut when n_videoInterfaceCS = '0' else
-	basRomData when n_basRomCS = '0' else
-	internalRam1DataOut when n_internalRamCS= '0' else
-	x"FF";
+	w_n_memWR <= not(w_cpuClock) nand (not w_n_WR);
 	
 	-- ____________________________________________________________________________________
 	-- SYSTEM CLOCKS GO HERE
 	-- SUB-CIRCUIT CLOCK SIGNALS
-process (clk)
-begin
-if rising_edge(clk) then
-
-if cpuClkCount < 4 then -- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
-cpuClkCount <= cpuClkCount + 1;
-else
-cpuClkCount <= (others=>'0');
-end if;
-if cpuClkCount < 2 then -- 2 when 10MHz, 2 when 12.5MHz, 2 when 16.6MHz, 1 when 25MHz
-cpuClock <= '0';
-else
-cpuClock <= '1';
-end if;
-
-end if;
-end process;
+process (i_clk_50)
+	begin
+		if rising_edge(i_clk_50) then
+			if w_cpuClkCount < 4 then -- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
+				w_cpuClkCount <= w_cpuClkCount + 1;
+			else
+				w_cpuClkCount <= (others=>'0');
+			end if;
+			if w_cpuClkCount < 2 then -- 2 when 10MHz, 2 when 12.5MHz, 2 when 16.6MHz, 1 when 25MHz
+				w_cpuClock <= '0';
+			else
+				w_cpuClock <= '1';
+			end if;
+		end if;
+	end process;
 end;
