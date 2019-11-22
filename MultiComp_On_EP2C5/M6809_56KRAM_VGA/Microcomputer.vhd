d32f70521b830256583108d781bbd6d7f1ade387
Doug Gilliland <doug@douglasgilliland.com>
2019-11-22 12:06:36 -0500

    Bumped CPU clock speed to 25 MHz

--- MultiComp_On_EP2C5/M6809_56KRAM_VGA/Microcomputer.vhd
+++ MultiComp_On_EP2C5/M6809_56KRAM_VGA/Microcomputer.vhd
@@ -3,8 +3,8 @@
 -- Grant Searle's "multicomp" page at http://searle.hostei.com/grant/Multicomp/index.html
 --
 -- Changes to this code by Doug Gilliland 2017-2019
--- 56K SRAM
--- Microsoft BASIC
+-- 56K External SRAM
+-- 8K Microsoft BASIC in ROM
 --
 
 library ieee;
@@ -233,12 +233,12 @@ begin
 		begin
 			if rising_edge(clk) then
 			
-			if cpuClkCount < 4 then -- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
+			if cpuClkCount < 4 then -- 1 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
 				cpuClkCount <= cpuClkCount + 1;
 			else
 				cpuClkCount <= (others=>'0');
 			end if;
-			if cpuClkCount < 2 then -- 2 when 10MHz, 2 when 12.5MHz, 2 when 16.6MHz, 1 when 25MHz
+			if cpuClkCount < 2 then -- 1 when 10MHz, 2 when 12.5MHz, 2 when 16.6MHz, 1 when 25MHz
 				cpuClock <= '0';
 			else
 				cpuClock <= '1';
