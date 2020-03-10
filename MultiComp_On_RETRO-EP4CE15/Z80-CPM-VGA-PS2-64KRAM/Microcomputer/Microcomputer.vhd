6f5039f53d9902b280c6f4a498de269b970bca64
Doug Gilliland <doug@douglasgilliland.com>
2020-03-09 21:19:55 -0400

    Faster SPI clock

--- MultiComp_On_RETRO-EP4CE15/Z80-CPM-VGA-PS2-64KRAM/Microcomputer/Microcomputer.vhd
+++ MultiComp_On_RETRO-EP4CE15/Z80-CPM-VGA-PS2-64KRAM/Microcomputer/Microcomputer.vhd
@@ -233,7 +233,7 @@ port map(
 	dataOut	=> sdCardDataOut,
 	regAddr	=> cpuAddress(2 downto 0),
 	driveLED	=> driveLED,
-	clk		=> sdClock -- twice the spi clk
+	clk		=> clk 
 );
 
 -- ____________________________________________________________________________________
@@ -279,12 +279,12 @@ cpuDataIn <=
 -- SUB-CIRCUIT CLOCK SIGNALS
 clk_gen: process (clk) begin
 	if rising_edge(clk) then
-		if cpuClkCount < 4 then -- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
+		if cpuClkCount < 1 then -- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
 			cpuClkCount <= cpuClkCount + 4;
 		else
 			cpuClkCount <= (others=>'0');
 		end if;
-		if cpuClkCount < 2 then -- 2 when 10MHz, 2 when 12.5MHz, 2 when 16.6MHz, 1 when 25MHz
+		if cpuClkCount < 1 then -- 2 when 10MHz, 2 when 12.5MHz, 2 when 16.6MHz, 1 when 25MHz
 			cpuClock <= '0';
 		else
 			cpuClock <= '1';
