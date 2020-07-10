0753fc56071e6c05b78e2bd88058502fa75a2236
Doug Gilliland <doug@douglasgilliland.com>
2020-07-10 14:08:37 -0400

    Longer S record lload timeout

--- MultiComp_On_RETRO-EP4CE15/TS2_68000_Tutor/db/ip/sldb3504235/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ MultiComp_On_RETRO-EP4CE15/TS2_68000_Tutor/db/ip/sldb3504235/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : b9a3b1965aa2c7567fed
+//   DESIGN_HASH   : d954f01eb1eb7e8912bb
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hb9a3b1965aa2c7567fed };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hd954f01eb1eb7e8912bb };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
