5fa3c30552ffc946fa62b04aea653aebe8ff4ab5
Doug Gilliland <doug@douglasgilliland.com>
2021-06-01 09:26:27 -0400

    Using explicitly named ROM file _60KB

--- MultiComp_On_RETRO-EP4CE15/M6800_MIKBUG_60KB_Banked-1MB/db/ip/sld7a317a57/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ MultiComp_On_RETRO-EP4CE15/M6800_MIKBUG_60KB_Banked-1MB/db/ip/sld7a317a57/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 0f5f00a01ac186936552
+//   DESIGN_HASH   : 46070b22e182f707bcb2
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h0f5f00a01ac186936552 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h46070b22e182f707bcb2 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
