5884c4c86718f611b7d9829c619af8162d90729a
Doug Gilliland <doug@douglasgilliland.com>
2021-06-15 06:25:49 -0400

    Removed Signal Tap - Working

--- MultiComp_On_RETRO-EP4CE15/FrontPanel01/db/ip/sldd3ad11c3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ MultiComp_On_RETRO-EP4CE15/FrontPanel01/db/ip/sldd3ad11c3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 8e1a5b2b7db5b001bce8
+//   DESIGN_HASH   : d77f1367e387230bb3a3
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8e1a5b2b7db5b001bce8 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hd77f1367e387230bb3a3 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
