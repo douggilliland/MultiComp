8e0cdabb550008faf798ffacdee3e59586b037c6
Doug Gilliland <doug@douglasgilliland.com>
2021-06-21 06:14:16 -0400

    Front Panel test using LOOP5 - 2 level stack - nested subroutines woking

--- MultiComp_On_RETRO-EP4CE15/FrontPanel01_Test_LoopBack/db/ip/sldd3ad11c3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ MultiComp_On_RETRO-EP4CE15/FrontPanel01_Test_LoopBack/db/ip/sldd3ad11c3/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : f0061b67211e570b1803
+//   DESIGN_HASH   : a10773cfcbbf89929dec
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hf0061b67211e570b1803 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha10773cfcbbf89929dec };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
