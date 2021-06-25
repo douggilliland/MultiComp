08e093d684e6db8959d8904248af86ffc6604cb2
Doug Gilliland <doug@douglasgilliland.com>
2021-06-25 10:02:38 -0400

    WORKING

--- MultiComp_On_EP2C5/M6800_MIKBUG_ExtSRAM_MIKBUG_at_F000/M6800_MIKBUG.vhd
+++ MultiComp_On_EP2C5/M6800_MIKBUG_ExtSRAM_MIKBUG_at_F000/M6800_MIKBUG.vhd
@@ -42,6 +42,7 @@ entity M6800_MIKBUG is
 		i_n_reset			: in std_logic := '1';
 		i_CLOCK_50			: in std_logic;
 
+		-- VGA
 		o_videoR0			: out std_logic := '1';
 		o_videoR1			: out std_logic := '1';
 		o_videoG0			: out std_logic := '1';
@@ -51,28 +52,30 @@ entity M6800_MIKBUG is
 		o_hSync				: out std_logic := '1';
 		o_vSync				: out std_logic := '1';
 
+		-- PS/2
 		io_ps2Clk			: inout std_logic := '1';
 		io_ps2Data			: inout std_logic := '1';
 		
+		-- Derial
 		i_rxd1				: in	std_logic := '1';
 		o_txd1				: out std_logic;
 		o_rts1				: out std_logic;
---		urts1					: in	std_logic := '1';
-		i_serSelect			: in	std_logic := '1';
+--		w_serSelect			: in	std_logic := '1';
 		
-		-- 128KB SRAM (32KB used)
+		-- 128KB SRAM (60KB used)
 		io_extSRamData		: inout std_logic_vector(7 downto 0) := (others=>'Z');
 		o_extSRamAddress	: out std_logic_vector(16 downto 0);
-		io_n_extSRamWE		: out std_logic := '1';
-		io_n_extSRamCS		: out std_logic := '1';
-		io_n_extSRamOE		: out std_logic := '1';
+		o_n_extSRamWE		: out std_logic := '1';
+		o_n_extSRamCS		: out std_logic := '1';
+		o_n_extSRamOE		: out std_logic := '1';
 		
+		-- I/O
 		o_ledDS1				: inout std_logic;
 		o_ledD2				: inout std_logic;
 		o_ledD4				: inout std_logic;
 		o_ledD5				: inout std_logic;
-		o_J6IO8				: inout std_logic_vector(7 downto 0);
-		o_J8IO8				: inout std_logic_vector(5 downto 0)
+		o_J6IO8				: inout std_logic_vector(7 downto 0) := (others=>'0');
+		J8IO8				: inout std_logic_vector(7 downto 0)
 	);
 end M6800_MIKBUG;
 
@@ -85,6 +88,8 @@ architecture struct of M6800_MIKBUG is
 	signal w_cpuDataIn	: std_logic_vector(7 downto 0);
 	signal w_R1W0			: std_logic;
 	signal w_vma			: std_logic;
+	signal w_memWR			: std_logic;
+	signal w_memRD			: std_logic;
 	
 	signal w_n_rts			: std_logic;
 
@@ -93,7 +98,7 @@ architecture struct of M6800_MIKBUG is
 	signal w_ACIADataOut	: std_logic_vector(7 downto 0);
 	
 	signal w_ExtRamAddr	: std_logic :='0';
-	signal w_IOSel			: std_logic :='0';
+--	signal w_IOSel			: std_logic :='0';
 	signal w_n_VDUint		: std_logic :='1';
 	signal n_vduCSN		: std_logic :='1';
 	signal w_n_ACIAint	: std_logic :='1';	
@@ -109,32 +114,27 @@ architecture struct of M6800_MIKBUG is
    signal w_serialCt   	: std_logic_vector(15 downto 0) := x"0000";
    signal w_serialCt_d	: std_logic_vector(15 downto 0);
    signal w_serialEn    : std_logic;
+	signal w_serSelect   : std_logic;
 	
 	signal w_J8IO8			: std_logic_vector(7 downto 0);
 	
 begin
 --	o_rts1	<= w_n_rts;
 --	o_ledDS1	<= w_n_rts;
+--	J8IO8 <= w_J8IO8(6 downto 0);
+	J8IO8(0)	<= w_cpuClock;				-- Pin 48
+	J8IO8(1)	<= w_memWR;					-- Pin 47
+	J8IO8(2)	<= w_memRD;					-- Pin 52
+	J8IO8(3)	<= w_vma;					-- Pin 51
+	J8IO8(4)	<= w_ExtRamAddr;			-- Pin 58
+	J8IO8(5)	<= w_resetLow;				-- Pin 55
+	J8IO8(6)	<= '0';
+	w_serSelect <= J8IO8(7);
 	
-	o_J8IO8 <= w_J8IO8(5 downto 0);
+--	o_J8IO8 <= w_J8IO8(5 downto 0);
 	
-	w_IOSel <= (w_cpuAddress(15) and w_cpuAddress(14) and      w_cpuAddress(13) and      w_cpuAddress(12) and 
-					w_cpuAddress(11) and w_cpuAddress(10) and (not w_cpuAddress(9)) and (not w_cpuAddress(8)));
-	
-	-- ____________________________________________________________________________________
-	-- RAM GOES HERE
-	w_ExtRamAddr <= ((not w_cpuAddress(15)) 										-- 0x0000 - 0x7FFF
-							or (w_cpuAddress(15) and (not w_cpuAddress(14)))	-- 0x8000 - 0XBFFF
-							or (w_cpuAddress(15) and (not w_cpuAddress(13)))	-- 0xC000 - 0xDFFF
-							or (w_cpuAddress(15) and (not w_cpuAddress(12)))	-- 0xE000 - 0xEFFF
-							);
-	
-	o_extSRamAddress	<= '0'&w_cpuAddress(15 downto 0);
-	io_extSRamData		<= w_cpuDataOut when ((w_R1W0='0') and (w_ExtRamAddr = '1')) else
-							  (others => 'Z');
-	io_n_extSRamWE		<= not(w_ExtRamAddr and w_vma and (not w_R1W0));
-	io_n_extSRamOE		<= not(w_ExtRamAddr and w_vma and      w_R1W0);
-	io_n_extSRamCS		<= not(w_ExtRamAddr and w_vma and (not w_cpuClock));
+--	w_IOSel <= (w_cpuAddress(15) and w_cpuAddress(14) and      w_cpuAddress(13) and      w_cpuAddress(12) and 
+--					w_cpuAddress(11) and w_cpuAddress(10) and (not w_cpuAddress(9)) and (not w_cpuAddress(8)));
 	
 	-- Debounce the reset line
 	DebounceResetSwitch	: entity work.Debounce
@@ -143,14 +143,31 @@ begin
 		button	=> i_n_reset,
 		result	=> w_resetLow
 	);
-		
+	
+	-- ____________________________________________________________________________________
+	-- External SRAM GOES HERE
+	w_memWR <= (not w_R1W0) and w_vma and w_cpuClock;
+	w_memRD <=      w_R1W0  and w_vma and w_cpuClock;
+	
+	w_ExtRamAddr <=  (not w_cpuAddress(15))										-- 0x0000 - 0x7FFF
+							or (w_cpuAddress(15) and (not w_cpuAddress(14)))	-- 0x8000 - 0XBFFF
+							or (w_cpuAddress(15) and (not w_cpuAddress(13)))	-- 0xC000 - 0xDFFF
+							or (w_cpuAddress(15) and (not w_cpuAddress(12)));	-- 0xE000 - 0xEFFF
+	
+	o_extSRamAddress	<= '0'&w_cpuAddress(15 downto 0);
+	io_extSRamData		<= w_cpuDataOut when ((w_R1W0='0') and (w_ExtRamAddr = '1')) else
+							  (others => 'Z');
+	o_n_extSRamWE		<= not(w_ExtRamAddr and w_memWR);
+	o_n_extSRamOE		<= not(w_ExtRamAddr and w_memRD);
+	o_n_extSRamCS		<= not(w_ExtRamAddr and w_vma);
+	
 	-- ____________________________________________________________________________________
 	-- I/O CHIP SELECTS
-	n_vduCSN	<= '0' 		when (i_serSelect = '1' and (w_cpuAddress(15 downto 1) = x"FC1"&"100")) else	-- VDU  $E018-$E019
-					'0'		when (i_serSelect = '0' and (w_cpuAddress(15 downto 1) = x"FC2"&"100")) else	-- ACIA $E028-$E029
+	n_vduCSN	<= '0' 		when (w_serSelect = '1' and (w_cpuAddress(15 downto 1) = x"FC1"&"100")) else	-- VDU  $E018-$E019
+					'0'		when (w_serSelect = '0' and (w_cpuAddress(15 downto 1) = x"FC2"&"100")) else	-- ACIA $E028-$E029
 					'1';
-	w_n_aciaCSN <= '0'	when (i_serSelect = '1' and (w_cpuAddress(15 downto 1) = x"FC2"&"100")) else	-- ACIA $E028-$E029
-					'0'		when (i_serSelect = '0' and (w_cpuAddress(15 downto 1) = x"FC1"&"100")) else	-- VDU  $E018-$E019
+	w_n_aciaCSN <= '0'	when (w_serSelect = '1' and (w_cpuAddress(15 downto 1) = x"FC2"&"100")) else	-- ACIA $E028-$E029
+					'0'		when (w_serSelect = '0' and (w_cpuAddress(15 downto 1) = x"FC1"&"100")) else	-- VDU  $E018-$E019
 					'1';
 	w_n_J8IOCS	<= '0' 	when (w_vma = '1') and (w_cpuAddress = x"FC30")	else	-- J8 I/O $8030
 					'1';
@@ -162,13 +179,13 @@ begin
 	-- ____________________________________________________________________________________
 	-- CPU Read Data multiplexer
 	w_cpuDataIn <=
-		io_extSRamData	when w_ExtRamAddr = '1'								else
-		w_romData		when ((w_cpuAddress(15 downto 12) = x"F") and w_IOSel = '0')	else
-		w_VDUDataOut	when n_vduCSN = '0'									else
-		w_ACIADataOut	when w_n_aciaCSN = '0'								else
-		w_ledDS18		when w_n_LEDCS = '0'									else
-		o_J6IO8			when w_n_J6IOCS = '0'								else
-		w_J8IO8			when w_n_J8IOCS = '0'								else
+		io_extSRamData	when w_ExtRamAddr = '1'						else
+		w_VDUDataOut	when n_vduCSN = '0'							else
+		w_ACIADataOut	when w_n_aciaCSN = '0'						else
+		w_ledDS18		when w_n_LEDCS = '0'							else
+		o_J6IO8			when w_n_J6IOCS = '0'						else
+		w_J8IO8			when w_n_J8IOCS = '0'						else
+		w_romData		when w_cpuAddress(15 downto 12) = x"F"	else	-- Always last to open hole fir I/O
 		x"FF";
 	
 	-- ____________________________________________________________________________________
@@ -214,8 +231,8 @@ begin
 			videoG1	=> o_videoG1,
 			videoB0	=> o_videoB0,
 			videoB1	=> o_videoB1,
-			n_WR		=> n_vduCSN or      w_R1W0  or (not w_vma) or (not w_cpuClock),
-			n_rd		=> n_vduCSN or (not w_R1W0) or (not w_vma),
+			n_wr		=> n_vduCSN or (not w_memWR),
+			n_rd		=> n_vduCSN or (not w_memRD),
 			n_int		=> w_n_VDUint,
 			regSel	=> w_cpuAddress(0),
 			dataIn	=> w_cpuDataOut,
@@ -228,8 +245,8 @@ begin
 	acia: entity work.bufferedUART
 		port map (
 			clk		=> i_CLOCK_50,     
-			n_WR		=> w_n_aciaCSN or      w_R1W0  or (not w_vma) or (not w_cpuClock),
-			n_rd		=> w_n_aciaCSN or (not w_R1W0) or (not w_vma),
+			n_wr		=> w_n_aciaCSN or (not w_memWR),
+			n_rd		=> w_n_aciaCSN or (not w_memRD),
 			regSel	=> w_cpuAddress(0),
 			dataIn	=> w_cpuDataOut,
 			dataOut	=> w_ACIADataOut,
@@ -248,7 +265,7 @@ begin
 	port map(
 		clear		=> w_resetLow,
 		clock		=> i_CLOCK_50,
-		load		=> not ((not w_n_J6IOCS) and (not w_R1W0) and w_cpuClock),
+		load		=> not ((not w_n_J6IOCS) and (w_memWR)),
 		dataIn	=> w_cpuDataOut,
 		latchOut	=> o_J6IO8
 	);
@@ -257,32 +274,32 @@ begin
 	port map(
 		clear		=> w_resetLow,
 		clock		=> i_CLOCK_50,
-		load		=> not ((not w_n_J8IOCS) and (not w_R1W0) and w_cpuClock),
+		load		=> not ((not w_n_J8IOCS) and w_memWR),
 		dataIn	=> w_cpuDataOut,
 		latchOut	=> w_J8IO8
 	);
 
 
-o_ledDS1		<= w_ledDS18(0);
-o_ledD2		<= not w_ledDS18(1);
-o_ledD4		<= not w_ledDS18(2);
-o_ledD5		<= not w_ledDS18(3);
+	o_ledDS1		<= w_ledDS18(0);
+	o_ledD2		<= not w_ledDS18(1);
+	o_ledD4		<= not w_ledDS18(2);
+	o_ledD5		<= not w_ledDS18(3);
 
-latchLED : entity work.OutLatch	--Output LatchIO
-port map(
-	clear		=> w_resetLow,
-	clock		=> i_CLOCK_50,
-	load		=> not ((not w_n_LEDCS) and (not w_R1W0) and w_cpuClock),
-	dataIn	=> w_cpuDataOut,
-	latchOut => w_ledDS18
-);
+	latchLED : entity work.OutLatch	--Output LatchIO
+	port map(
+		clear		=> w_resetLow,
+		clock		=> i_CLOCK_50,
+		load		=> not ((not w_n_LEDCS) and w_memWR),
+		dataIn	=> w_cpuDataOut,
+		latchOut => w_ledDS18
+	);
 
 	-- ____________________________________________________________________________________
 	-- CPU Clock
 process (i_CLOCK_50)
 	begin
 		if rising_edge(i_CLOCK_50) then
-			if w_cpuClkCt < 2 then		-- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
+			if w_cpuClkCt < 4 then		-- 4 = 10MHz, 3 = 12.5MHz, 2=16.6MHz, 1=25MHz
 				w_cpuClkCt <= w_cpuClkCt + 1;
 			else
 				w_cpuClkCt <= (others=>'0');
